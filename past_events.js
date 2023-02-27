156e737ccdad867008b9ca439a127bf4e58672a1
Salvador Acuña <padrecontratgd@gmail.com>
2023-02-26 21:54:37 -0300

    hice cambios de estilo y cree past events js para manejar dinammicamente la past events html

---
+++ past_events.js
@@ -0,0 +1,21 @@
+
+let divContainerEvents = document.getElementById('div-container-events');
+let fragment = document.createDocumentFragment()
+for (const event of tarjetas.events) {
+  
+  let cardUnica = document.createElement('div')
+  cardUnica.classList.add('card', 'text-center')
+  cardUnica.innerHTML = `<img src="${event.image}" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">${event.name}</h5>
+        <p class="card-text">
+          ${event.description}
+        </p>
+        <div class="price-and-button d-flex justify-content-between">
+          <p class="card-text">PRICE $ ${event.price}</p>
+          <a href="./details.html" class="btn" style="background-color: #a493e6"><i>VAMOS</i></i></a>
+        </div>
+        `
+  fragment.appendChild(cardUnica)
+}
+divContainerEvents.appendChild(fragment)
